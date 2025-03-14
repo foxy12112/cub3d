1417caa0c5a39ac35b2d1e902e2b634434737696
Leon Dick <ldick@student.42heilbronn.de>
2025-03-14 17:40:50 +0100

    hi

--- srcs/parsing/map_parse_utils.c
+++ srcs/parsing/map_parse_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_parse_utils.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: petrasostaricvulic <petrasostaricvulic@    +#+  +:+       +#+        */
+/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/10 09:34:40 by petrasostar       #+#    #+#             */
-/*   Updated: 2025/03/10 11:22:14 by petrasostar      ###   ########.fr       */
+/*   Created: 2025/01/25 17:35:29 by ldick             #+#    #+#             */
+/*   Updated: 2025/03/07 13:16:25 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,109 +14,134 @@
 
 int	check_top(t_cub_data *cub)
 {
-	int	i = 0;
+	int	i;
 
-	while (cub->map[0][i] && (cub->map[0][i] == ' ' || cub->map[0][i] == '\t'))
-		i++;
-
+	i = 0;
 	while (cub->map[0][i])
 	{
-		if (cub->map[0][i] != '1' && cub->map[0][i] != ' ')
+		if (cub->map[0][i] != ' ' && cub->map[0][i] != '1')
 			return (ft_error(cub, "top not closed"), 1);
 		i++;
 	}
-
-	i--;
-	while (i >= 0 && (cub->map[0][i] == ' ' || cub->map[0][i] == '\t'))
-		i--;
-
-	if (cub->map[0][i] != '1')
-		return (ft_error(cub, "top not closed"), 1);
-
 	return (0);
 }
 
 int	check_bottom(t_cub_data *cub)
 {
-	int	x = 0;
-	int	y = 0;
+	int	x;
+	int	y;
 
+	x = 0;
+	y = 0;
 	while (cub->map[y])
 		y++;
 	y--;
-
-	while (cub->map[y][x] && (cub->map[y][x] == ' ' || cub->map[y][x] == '\t'))
-		x++;
-
 	while (cub->map[y][x])
 	{
-		if (cub->map[y][x] != '1' && cub->map[y][x] != ' ')
+		if (cub->map[y][x] != ' ' && cub->map[y][x] != '1')
 			return (ft_error(cub, "bottom not closed"), 1);
 		x++;
 	}
-
-	x--;
-	while (x >= 0 && (cub->map[y][x] == ' ' || cub->map[y][x] == '\t'))
-		x--;
-
-	if (cub->map[y][x] != '1')
-		return (ft_error(cub, "bottom not closed"), 1);
-
 	return (0);
+}
+
+void printMap(char **map) {
+    for (int i = 0; map[i] != NULL; i++) { // Iterate until a NULL pointer is found
+        printf("%s\n", map[i]); // Print each row (string)
+    }
+}
+
+// int	check_overhangs(t_cub_data *cub)
+// {
+// 	int	row1;
+// 	int	row2;
+// 	int	y;
+// 	int	x;
+
+// 	x =0 ;
+// 	row1 = 0;
+// 	row2 = 0;
+// 	y = 0;
+// 	while(cub->map[y])
+// 	{
+// // dprintf(2, "----------------------------------------------------------------------------------------------------------\n");
+// 		if (row1 == 0)
+// 		{
+// 			row1 = ft_strlen(cub->map[y]);
+// 			y++;
+// 			row2 = ft_strlen(cub->map[y]);
+// 		}
+// 		if (row1 > row2)
+// 		{
+// 			x = row2;
+// 			printf("%d--%d\n", y, x);
+// 			while(cub->map[y][x])
+// 			{
+// 				if (x >= row1 || !cub->map[y][x])
+// 					break ;
+// 				if (cub->map[y][x] != '1' && cub->map[y][x] != ' ')
+// 				{
+// 					return (ft_error(cub, "map is not closed"), 1);
+// 				}
+// 				x++;
+// 			}
+// 		}
+// // dprintf(2, "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++\n");
+// 		row1 = ft_strlen(cub->map[y]);
+// 		y++;
+// 		row2 = ft_strlen(cub->map[y]);
+// 	}
+// 	return (0);
+// }
+
+// void	floodfill(char **map, int x, int y)
+// {
+
+// 	if (map && y >= 0 && map[y] && x >= 0 && x <= (int)ft_strlen(map[y]) && map[y][x] && map[y][x] != '0' && map[y][x] != '*')
+// 	{
+// 		map[y][x] = '*';
+// 		floodfill(map, x, y + 1);
+// 		floodfill(map, x - 1, y);
+// 		floodfill(map, x, y - 1);
+// 		floodfill(map, x + 1, y);
+// 	}
+// }
+
+int		check_leaks(char **map, t_cub_data *cub)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	y = 0;
+	while(map[y])
+	{
+		while(map[y][x])
+		{
+			if (map[y][x] == '0')
+			{
+				if (!map[y - 1] || !map[y + 1] || !map[y - 1][x] || !map[y + 1][x])
+					return (ft_error(cub, "overhang not closed"), false);
+			}
+			x++;
+		}
+		y++;
+		x = 0;
+	}
+	return (true);
 }
 
 int	check_sides(t_cub_data *cub)
 {
 	int	y;
-	int	x;
 
 	y = 0;
 	while (cub->map[y])
 	{
-		// Find the first visible character in the row (skip spaces and tabs)
-		x = 0;
-		while (cub->map[y][x] && (cub->map[y][x] == ' ' || cub->map[y][x] == '\t'))
-			x++;
-
-		// The first visible character must be '1'
-		if (cub->map[y][x] != '1')
+		if ((cub->map[y][0] != ' ' && cub->map[y][0] != '1') || (cub->map[y][ft_strlen(cub->map[y]) - 1] != ' ' && cub->map[y][ft_strlen(cub->map[y]) - 1] != '1'))
 			return (ft_error(cub, "sides not closed"), 1);
-
-		// Find the last visible character in the row
-		x = ft_strlen(cub->map[y]) - 1;
-		while (x >= 0 && (cub->map[y][x] == ' ' || cub->map[y][x] == '\t'))
-			x--;
-
-		// The last visible character must be '1'
-		if (cub->map[y][x] != '1')
-			return (ft_error(cub, "sides not closed"), 1);
-
 		y++;
 	}
 	return (0);
 }
-
-int check_map_validity(t_cub_data *cub)
-{
-    // Check for NULL pointers (ensure that the map and other data exist)
-    if (!cub || !cub->map) 
-	{
-        return ft_error(cub, "Invalid map data"), FAILURE;  // Return error if the map data is missing or invalid
-    }
-
-    // Check if the map has minimum dimensions (height and width)
-    if (cub->map_height < 3 || cub->map_width < 3) 
-	{
-        return ft_error(cub, "Map is too small"), FAILURE; // Return error if the map is smaller than 3x3
-    }
-
-    // You can add other checks, such as verifying that the map contains the necessary elements (e.g., walls, player, etc.)
-    if (check_invalid_chars(cub) == FAILURE) 
-	{
-        return ft_error(cub, "Map elements are invalid"), FAILURE;  // Return error if the map elements (like walls, empty spaces, etc.) are incorrect
-    }
-
-    return (SUCCESS);  // Return success if all checks pass
-}
-
 
--- srcs/parsing/parsing.c
+++ srcs/parsing/parsing.c
@@ -3,75 +3,37 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: petrasostaricvulic <petrasostaricvulic@    +#+  +:+       +#+        */
+/*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 18:55:37 by ldick             #+#    #+#             */
-/*   Updated: 2025/03/10 08:12:30 by petrasostar      ###   ########.fr       */
+/*   Updated: 2025/02/12 17:54:04 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int parsing(t_cub_data *cub)
+// void	minimap_size(t_cub_data *cub)
+// {
+// 	int	y;
+
+// 	y = 0;
+// 	cub->minimap->size_x = 0;
+// 	while (cub->map[y])
+// 	{
+// 		if (y > 0 && cub->map[y + 1] != NULL)
+// 			if ((int)ft_strlen(cub->map[y]) >= cub->minimap->size_x)
+// 				cub->minimap->size_x = ft_strlen(cub->map[y]);
+// 		y++;
+// 	}
+// 	cub->minimap->size_y = y;
+// }
+
+int	parsing(t_cub_data *cub)
 {
-    // We check the folder first - if it exists, if the player is present, and if it is closed
-    if (check_map(cub) != 0)
-        return ft_error(cub, "Error in map parsing"), 1;
-
-    // We check the textures after ensuring that the map exists and is correct
-    if (check_textures(cub) != 0)
-        return ft_error(cub, "Error in texture parsing"), 1;
-
-    // We check the length of the lines on the map
-    if (check_row(cub) != 0)
-        return ft_error(cub, "Inconsistent row lengths in map"), 1;
-
-    // We are checking for invalid characters on the map
-    if (check_invalid_chars(cub) != 0)
-        return ft_error(cub, "Invalid character in map"), 1;
-
-    // We are checking the minimum folder size
-    if (check_map_dim(cub) != 0)
-        return ft_error(cub, "Map is too small, must be at least 3x3"), 1;
-
-    return (SUCCESS);
-}
-
-int check_invalid_chars(t_cub_data *cub)
-{
-    int y = 0;
-    int x;
-
-    while (cub->map[y]) {
-        x = 0;
-        while (cub->map[y][x]) {
-            if (cub->map[y][x] != '1' && cub->map[y][x] != '0' && cub->map[y][x] != 'N' && 
-                cub->map[y][x] != 'S' && cub->map[y][x] != 'E' && cub->map[y][x] != 'W' &&
-                cub->map[y][x] != ' ' && cub->map[y][x] != '\t'){
-                return ft_error(cub, "Invalid character in map"), FAILURE;
-            }
-            x++;
-        }
-        y++;
-    }
-    return (0);
-}
-
-int check_row(t_cub_data *cub)
-{
-    int y = 0;
-    int row_length = ft_strlen(cub->map[0]);
-
-    while (cub->map[y])
-    {
-        // If the current line is not the same length as the first, return an error
-        if ((int)ft_strlen(cub->map[y]) != row_length)
-        {
-            ft_error(cub, "Inconsistent row lengths in map");
-            return 1;
-        }
-        y++;
-    }
-
-    return (0);
+	if (check_textures(cub) == 1)
+		ft_error(cub, "error in textures");
+	if (check_map(cub) == 1)
+		ft_error(cub, "error in map parsing");
+	// minimap_size(cub);
+	return (0);
 }
