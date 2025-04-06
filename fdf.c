8d2c778e416d30b2f602b6485d562412b20de9f2
Leon Dick <ldick@student.42heilbronn.de>
2025-04-06 17:16:18 +0200

    helping alex with her fdf

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: ldick <ldick@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/04 18:32:50 by ldick             #+#    #+#             */
-/*   Updated: 2025/04/04 19:10:11 by ldick            ###   ########.fr       */
+/*   Updated: 2025/04/06 14:55:41 by ldick            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,15 @@
 #include <stdio.h>
 #include "main-libs/libs.h"
 
+#define MAX_WIDTH 100
+
 typedef struct	s_3d
 {
 	int			x;
 	int			y;
 	int			z;
 	int			color_val;
+	int			size;
 }				t_3d;
 
 typedef struct	s_fdf
@@ -33,15 +36,25 @@ typedef struct	s_fdf
 
 int	check_for_comma(char *line)
 {
+	printf("%s\n", line);
 	for (int i = 0; line[i]; i++)
 		if (line[i] == ',')
 			return (1);
 	return (0);
 }
 
+int	get_size(char **array)
+{
+	int count = 0;
+	while(array && array[count])
+		count++;
+	return (count);
+}
+
 t_3d *special_split(t_3d *fdf, char *line, int row)
 {
 	char **split = ft_split(line, ' ');
+	// printf("%s\n", line);
 	int i = 0;
 	while(split[i])
 	{
@@ -52,6 +65,7 @@ t_3d *special_split(t_3d *fdf, char *line, int row)
 			fdf[i].y = row;
 			fdf[i].z = atoi(diff_split[0]);
 			fdf[i].color_val = atoi(diff_split[1]);
+			fdf[i].size = get_size(split);
 			free(diff_split[0]);
 			free(diff_split[1]);
 			free(diff_split);
@@ -62,45 +76,75 @@ t_3d *special_split(t_3d *fdf, char *line, int row)
 			fdf[i].y = row;
 			fdf[i].z = atoi(split[i]);
 			fdf[i].color_val = 0xff0000ff;
+			fdf[i].size = get_size(split);
 		}
 		free(split[i]);
+		i++;
+		if (split[i] == NULL)
+			break ;
 	}
 	free(split);
-	free(line);
+	return (fdf);
 }
+
 
 void	print(t_3d **three_d, int row)
 {
 	FILE *file = fopen("fdf_log.txt", "w");
 	int i = 0;
 	int j = 0;
-	while(three_d[i])
+	while(i < row)
 	{
-		while(three_d[i][j])
+		while(three_d[i][j].size > j)
 		{
-			
+			// printf("%d--j-%d\n", three_d[i][j].size, j);
+			fprintf(file, "%d , %d\t", three_d[i][j].x, three_d[i][j].color_val);
+			j++;
 		}
-		fprintf(file, "%d, %d")
+		fprintf(file, "\n");
+		i++;
+		j = 0;
+		// fprintf(file, "%d, %d")
 	}
+	fclose(file);
+}
+
+int get_rows(char *file)
+{
+	int fd = open(file, O_RDONLY);
+	char *line = get_next_line(fd);
+	int i = 0;
+	while(line)
+	{
+		free(line);
+		line = get_next_line(fd);
+		i++;
+	}
+	free(line);
+	close(fd);
+	return (i);
 }
 
 int main(int argc, char *argv[])
 {
 	if (argc != 2)
 		return (0);
-	t_fdf *fdf = malloc(sizeof(fdf));
+	remove("fdf_log.txt");
+	int row = get_rows(argv[1]);
+	t_fdf *fdf = malloc(sizeof(t_fdf));
+	fdf->three_d = malloc(sizeof(t_3d) * row);
+	for(int i = 0; i < row; i++)
+		fdf->three_d[i] = malloc(sizeof(t_3d) * MAX_WIDTH);
 	int fd = open(argv[1], O_RDONLY);
 	char *line = get_next_line(fd);
 	int i = 0;
-	while(!line)
+	while(i < row && line)
 	{
-		fdf->three_d[i] = malloc(sizeof(t_3d));
-		if (!fdf->three_d[i])
-			return (0);
-		
-		fdf->three_d[i] = special_split(fdf, line, i);
+		fdf->three_d[i] = special_split(fdf->three_d[i], line, i);
 		line = get_next_line(fd);
+		i++;
 	}
-	free(line);
-	print(fdf->three_d);
+	print(fdf->three_d, row);
+	// printf("atoi of hex number = %d", atoi("0xFFFFFF");)
+	return (0);
 }
